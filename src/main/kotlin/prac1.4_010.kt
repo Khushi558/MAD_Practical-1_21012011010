6607c896c901f95ad87954a457163ae593762bf6
student <akpathan560@gmail.com>
2023-07-21 14:53:20 +0530

    practical-1

--- src/main/kotlin/prac1.4_010.kt
+++ src/main/kotlin/prac1.4_010.kt
@@ -1,6 +1,6 @@
 fun main(){
     print("Enter a number: ")
-    val num=readLine()!!
+    val num=Integer.valueOf(readLine())
     if (num % 2==0)
         println("num is even")
     else
---
+++ src/main/kotlin/prac1.7.kt
@@ -0,0 +1,20 @@
+fun factorial(x:Int):Int {
+    if (x == 0) {
+        return 1
+    } else {
+        return x * factorial(x - 1)
+    }
+}
+tailrec fun fact(n: Int, temp: Int = 1): Int {
+    return if (n == 1){
+        temp
+    } else {
+        fact(n-1, temp*n)
+    }
+}
+fun main(){
+    print("Enter Number:")
+    val num=readLine()!!.toInt()
+    println("factorial of $num is ${factorial(num)}")
+    println("factorial of $num by using tailrec keyword is:${fact(num)}")
+}
